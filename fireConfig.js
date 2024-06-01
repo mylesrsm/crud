606eea2801d1ebeead7f46cd0a119e34359ee275
mylesrsm <163975806+mylesrsm@users.noreply.github.com>
2024-06-01 23:46:34 +0800

    initial commit

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBOPR8NFX2qQ-gjvScjNZFmncxUl2rJi6o",
+  authDomain: "crud-fd418.firebaseapp.com",
+  databaseURL: "https://crud-fd418-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "crud-fd418",
+  storageBucket: "crud-fd418.appspot.com",
+  messagingSenderId: "314336192261",
+  appId: "1:314336192261:web:b831b92a7de124361c396c"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
